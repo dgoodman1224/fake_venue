455e732af536d6e50e02bbe2b7b9e377ec82d03f
David Goodman <goodman.david.h@gmail.com>
2014-04-10 15:30:05 -0700

    update work

--- fake_venue/app/models/event.rb
+++ fake_venue/app/models/event.rb
@@ -1,4 +1,13 @@
 class Event < ActiveRecord::Base
 	validates :title, uniqueness: true
 	validates :price, :presence => {:message => 'Price cannot be blank, Task not saved'}
+	before_save :valid_date
+	def self.messages(error)
+		messages = {title: "The title must be unique", price: "The price must be listed"}
+		messages[error]
+	end
+
+	def valid_date
+		(/\d{2}\/\d{2}\/\d{4}/).match(self.date) == !nil ? true : false
+	end
 end
