f006f81eef0f6679acc20533bdda7afa89fd16bf
David Goodman <goodman.david.h@gmail.com>
2014-04-11 13:55:13 -0700

    javascript adds supporting artists

---
+++ fake_venue/app/assets/javascripts/supporting_artists.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+	var i = 0
+	var acts = ["#artist_second", "#artist_third", "#artist_fourth"]
+		addAct = function() {
+			$(acts[i]).show()
+			i+=1
+			if (i > 2) {
+				$('#supporting').hide()
+			}
+		}
+	$('#supporting').on("click", addAct)
+	});
