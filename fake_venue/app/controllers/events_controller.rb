064162a9707180eae916309729504fd67aaffd4d
David Goodman <goodman.david.h@gmail.com>
2014-04-18 16:01:50 -0700

    removes whitespace

--- fake_venue/app/controllers/events_controller.rb
+++ fake_venue/app/controllers/events_controller.rb
@@ -1,36 +1,23 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /events
-  # GET /events.json
+
   def index
     @events = Event.all.order(:date)
   end
 
-  # GET /events/1
-  # GET /events/1.json
+
   def show
   end
 
-  # GET /events/new
   def new
     @event = Event.new
     @errors = @errors
   end
 
-  # GET /events/1/edit
   def edit
-    @event = Event.find(params[id])
-    puts "@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@"
-    puts "@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@"
-    puts "@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@"
-    puts "@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@"
-    @errors = @event.errors
-    puts @errors
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
     if @event.save
@@ -41,8 +28,7 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
+
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -55,8 +41,7 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
+
   def destroy
     @event.destroy
     respond_to do |format|
@@ -66,12 +51,11 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:title, :artist, :artist_second, :artist_third, :artist_fourth, :date, :price)
     end
