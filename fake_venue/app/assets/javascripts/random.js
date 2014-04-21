07cba0917daecf612abb5064f910f22c34a2031d
David Goodman <goodman.david.h@gmail.com>
2014-04-21 16:56:37 -0700

    push before send

--- fake_venue/app/assets/javascripts/random.js
+++ fake_venue/app/assets/javascripts/random.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 var bands = ["Modest Mouse", "Johnny Cash", "Nicolas Jaar", "Gorillaz", "A Tribe Called Quest", 
 "London Souls", "Brandt, Bauer, Frick", "Ghostland Observatory", "The Twelves", "Soul Rebels Brass Brand", "Holy Ghost!", "Cut Copy",
-]
+"PHJB", "Dale Earnhardt Jr. Jr.", "Kasper Bjork"]
 
 $('#random_band').on('click', function(event) {
 	event.preventDefault();
